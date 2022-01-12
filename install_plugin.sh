b82cbf877163b2ddb5f749f4156e9093d14db314
nicolaas <nweidema@usc.edu>
2022-01-12 12:00:27 -0800

    Update install script to create .ghidra directory if it does not exist

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,2 +1,3 @@
 unzip -d /home/user/ghidra/Ghidra/Extensions/ $(ls -t dist/* | head -n1)
+mkdir -p /home/user/.ghidra
 cp smellcps_plugin_config.properties ~/.ghidra/smellcps_plugin_config.properties
